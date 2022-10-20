5c89efcf04a39d43a7a4ca4233de715a1e38e380
Slynyr <99285452+Slynyr@users.noreply.github.com>
2022-10-19 20:36:09 -0400

    MacAddress Management
    - Wrote functions for managing mac addresses in UDP mode
    (not functional)

--- MainLoop/P2PConnect.h
+++ MainLoop/P2PConnect.h
@@ -7,49 +7,80 @@
 
 #include "WiFi.h"
 #include "esp_now.h"
+
+//please dont shoot me - sincerely, codemonkey
+#include <vector>
+#include <string>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
 //GLOBALS
 const byte maxUsers = 16;
 unsigned int previousMillis;
 unsigned int currentMillis = millis();
-unsigned char * activeList[maxUsers] = {0}; 
-unsigned int timoutTime = 1; 
+std::vector<std::string> activeMacAddressList;
+std::vector<std::string> rollingMacAddressList;
+unsigned int timeoutTime = 1; 
 
 
 //--------------ActiveMacAddressTracking
-void updateActiveList(char * active, char * rolling, char * macAddrIn){
-  //check if the mac address passed in is present in activelist. If not then insert in both active and rolling.
-  bool presentInList = false; 
+void updateActiveList(const std::str& active, const std::str& rolling, char * macAddrIn){
+  //calls isAddressInList in order to determine if given macAddress is already present. If not, it is added to both the active and rolling list
+  //---activelist
+  if (!isAddressInList(activeMacAddressList, macAddrIn)){
+    activeMacAddressList.push_back(macAddrIn);
+  }
+  //---rolling list
+  if (!isAddressInList(rollingMacAddressList, macAddrIn)){
+    rollingMacAddressList.push_back(macAddrIn);
+  }
+}
 
-  for (int i = 0; i < sizeof(active); i++){
-    if (macAddrIn == active[i]){
-      presentInList = true;
+void isAddressInList(const std::str& listIn, char * macAddrIn){
+  //checks if a string is present in a list of strings. In this case it checks if a macaddress is present in the given list
+  bool isAddressFound false; 
+
+  for (auto listEntry : macAddrIn){ //iterates through list elements as "listEntry". If the element matches the madAddress testcase it returns a true state else the function will return a false state
+    if (listEntry == macAddrIn){
+      isAddressFound = true;
       break;
     }
-  } if (!presentInList) {
-    active[findArraySlot(active)] = macAddrIn;
   }
-  rolling[findArraySlot(rolling)] = macAddrIn;
+  return isAddressFound;
 }
 
-void findArraySlot(char * array){
-  //find empty slot in array and RETURN position of said slot. If no empty slot return a preset value to indicate that there is no space 
-  bool foundSlot = false;
-  int failState = -1;
-
-  for (int i = 0; i < sizeof(array); i++){
-    if (array[i] == 0){
-      foundSlot = true;
-      return i; 
+void clearList(const std::str& listIn){
+  //may not function
+  if (!listIn.empty()){
+    for (listElement : listIn){
+      listIn.erase(listElement);
     }
   }
-  return failState;
 }
 
-void dropArrayElement(char * array, int slot){
-  //drop element of said index/slot in array that is passed in
-  array[slot] = 0;
+
+void compareActiveRollingLists(const std::str& active, const std::str& rolling){
+  //Iterates through each active list element and checks if it is present in rolling list. If not present, it is poped. 
+  if ((currentDisplayMillis - previousMillis) >= (timeoutTime * 1000)){
+    for (auto activeElement : active){
+      bool inRolling = false;
+      for (auto rollingElement : active){
+        if (rollingElement == activeElement){
+          inRolling = true;
+        }
+      }
+      if (!inRolling){
+        active.erase(activeElement); 
+      }
+    }
+    previousMillis = currentDisplayMillis;
+    clearList(rolling)
+  }
 }
 
+/*
+SAFEKEEPING FOR LOGIC
 void compareArrayContents(char * active, char * rolling){ //name of function may be changed since its not entirely descriptive of what its doing
   //on millis timer. Run every X amount of seconds (effectively max timout length)
   //compare  the contents of activelist and rollinglist. remove any mac addresses that appear in active list but not in rolling 
@@ -71,8 +102,7 @@ void compareArrayContents(char * active, char * rolling){ //name of function may
     }
   }
 }
-
-
+*/
 
 void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength)
 //Formats MAC Address
